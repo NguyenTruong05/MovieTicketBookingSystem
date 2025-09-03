92424f9eeca2857192237fe07692b04f9312701a
NguyenTruong05 <truongdinhnguyen2005@gmail.com>
2025-09-03 21:15:03 +0700

    update

--- CinemaClient/Services/NetworkService.cs
+++ CinemaClient/Services/NetworkService.cs
@@ -49,10 +49,4 @@ namespace CinemaClient.Services
             _currentUser = username;
         }
 
-        public void Disconnect()
-        {
-            _stream?.Close();
-            _tcpClient?.Close();
-        }
-    }
-}
+        
